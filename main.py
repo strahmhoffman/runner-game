e32631fa2cb019c7fede62495c9785b6609f4c2e
strahmhoffman <strahmhoffman@gmail.com>
2021-12-16 21:41:54 +0500

    Drawing background surfaces
    1. Sky surface
    2. Ground surface
    3. Text surface

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pygame
 from sys import exit
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 800, 400
 
 if __name__ == '__main__':
 	pygame.init()
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption('Runner')
 	clock = pygame.time.Clock()
+	test_font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
+
+	sky_surface = pygame.image.load('assets/graphics/Sky.png')
+	ground_surface = pygame.image.load('assets/graphics/ground.png')
+	text_surface = test_font.render('My game', False, 'Black')
 
 	while True:
 		for event in pygame.event.get():
@@ -15,5 +20,10 @@ if __name__ == '__main__':
 				pygame.quit()
 				exit()
 
+		screen.blit(sky_surface, (0, 0))
+		screen.blit(ground_surface, (0, 300))
+		screen.blit(text_surface, (300, 50))
+
 		pygame.display.update()
+
 		clock.tick(60)
